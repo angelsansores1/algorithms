7dd504460923b816569afc3db761e23e46556474
angelsansores <2109139@upy.edu.mx>
2022-02-08 08:46:55 -0600

    nuevas tareas

---
+++ unit/myprogram.c
@@ -0,0 +1,9 @@
+#include <stdio.h> // External Library
+#include "utils.h" 
+
+int main(){ 
+    int value = isGreaterThan10(1);
+    printf("is greater than 10?: %d\n", value);
+
+return 0;
+}
---
+++ unit/utils.h
@@ -0,0 +1,12 @@
+
+
+int isGreaterThan10(int number){
+
+    int resultado= 0;
+    if (number > 10){
+        resultado = 1;
+    } else { resultado = 2; 
+    
+    }
+return resultado; 
+}
