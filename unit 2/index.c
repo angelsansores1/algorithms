27ac5e1c2ea833a09a562e78491fe058d3d607ce
root <root@LAPTOP-6LPJCNS9.localdomain>
2022-03-09 21:13:44 -0600

    micod

--- unit 2/index.c
+++ unit 2/index.c
@@ -3,9 +3,7 @@
 int myArray [ 3] = {5,  2, 3};
 int x = 0; 
 
-
 int main (){
-printf("[");
 void printArray (int myArray[3], int arrayLength){
 
 while (x < 3){
@@ -17,10 +15,7 @@ while (x < 3){
  }
 
 }
- 
-
 printArray (myArray, 3);
-printf("]\n");
 
     return 0;
 }
