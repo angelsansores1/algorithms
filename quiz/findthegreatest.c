5fb6e9d8e931cd0dc9ee5b39f27955d8a583f7cd
root <root@LAPTOP-6LPJCNS9.localdomain>
2022-04-08 17:16:14 -0500

    new

--- quiz/findthegreatest.c
+++ quiz/findthegreatest.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
-
 int higher(int arr[], int len){
-    int h;
-    for (int x =0; x < len -1; x++){
-        if (arr[x]> arr [x+1]){
-            h = arr[x];
+    for ( int a = 0;a < len; a++){
+        for (int x = 0 ;x < len -1; x ++){
+            if(arr[x] > arr[x+1]){
+                int num1 = arr[x];
+                arr[x] = arr[x+1];
+                arr[x+1] = num1;
+            }
         }
-        else{
-            h = arr[x+1];
     }
-    }
+    int h = arr[len-1];
+        
     return h;
 }
 
-int main(){
-    int arr[] = {0,0,0};
-    int len = sizeof(arr)/sizeof(int);
-    printf("put the numbers: \n");
-    scanf("%i%i%i", &arr[0],&arr[1],&arr[2]);
-    int max = higher(arr,len);
+int main(){int arr[] = {0,0,0};int len = sizeof(arr)/sizeof(int);
+
+    printf("tyoe 3 numbers: \n");
+        scanf("%i%i%i", &arr[0],&arr[1],&arr[2]);
+int max = higher(arr,len);
     printf("The greates is: %d \n", max);
     return 0;
 }
